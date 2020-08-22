bb1d6635f7c27fa9345d60016505315c8054c0c8
perseus0832 <nickvo2308@gmail.com>
2020-08-22 21:19:38 +0300

    finish anecdote exercise

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,22 +1,69 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({handelClick, text}) => {
+const Button = ({handleClick, text}) => {
+  return(
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Vote = ({value}) => {
+  console.log("value", value)
   return(
     <div>
-      <button onClick={handelClick}>{text}</button>
+      has {value} vote(s)
     </div>
   )
 }
 
+const AnecdoteDisplay = ({text, header, value}) => {
+  return(
+    <div>
+      <h1>{header}</h1>
+      {text}
+      <Vote value={value}/>
+    </div>
+  )
+}
+
+const MostVotedAnecdote = ({anecdotes, voted}) => {  
+  const maxVote = Math.max(...voted)
+  let maxVoteIdx = 0;
+  if (maxVote !== 0) {
+     maxVoteIdx = voted.indexOf(maxVote); 
+     return (
+        <AnecdoteDisplay 
+          text={anecdotes[maxVoteIdx]} 
+          header={'Anecdote with most votes'} 
+          value={maxVote}
+        />
+      );
+  }
+  return (
+    <div>
+      <br></br>
+      There are no vote yet
+    </div>
+  );
+}
+
 const App = (props) => {
+  //const points = [1, 4, 6, 3]
+  const points = new Array(props.anecdotes.length + 1).join('0').split('').map(parseFloat) 
+
   const [selected, setSelected] = useState(0)
+  const [voted, setVoted] = useState(points)
 
   const nextAnecdote = () => {
     let rand = getRandomInt(0,5)
-    console.log(rand)
     setSelected(rand)
-    console.log("select",selected)
+  }
+
+  const handleVote = () => {
+    const copyPoints = [...voted];
+    copyPoints[selected] += 1
+    setVoted(copyPoints)
+    console.log("selected",selected, voted)
   }
 
   const getRandomInt = (min, max) => {
@@ -27,9 +74,16 @@ const App = (props) => {
 
   return (
     <div>
-      {props.anecdotes[selected]}
-      <Button handelClick={nextAnecdote} text="next"/>
-    </div>
+        <AnecdoteDisplay 
+          text={props.anecdotes[selected]} 
+          header={'Anecdote of the day'} 
+          value={voted[selected]}
+        />
+        <br></br>
+        <Button handleClick={() => nextAnecdote()} text="next"/>
+        <Button handleClick={() => handleVote()} text="vote"/>
+        <MostVotedAnecdote anecdotes={props.anecdotes} voted={voted}/>
+    </div> 
   )
 }
 
